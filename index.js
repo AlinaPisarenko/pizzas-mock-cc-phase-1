31a973c58d7d5975f126ba9918cc5b9d0a114e95
flatlandnick <nicholas.lunn@flatironschool.com>
2022-06-15 16:03:55 -0500

    solution

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function renderPizzas(pizzas) {
 const pizzaMenu = document.querySelector("#pizza-menu");
 
 function renderPizza(pizza) {
+  console.log(pizza);
   let pizzaDiv = document.createElement("div");
   pizzaDiv.id = "pizza";
   let pizzaImg = document.createElement("img");
@@ -20,6 +21,18 @@ function renderPizza(pizza) {
   let likesDiv = document.createElement("div");
   likesDiv.id = "likes";
   let likeButton = document.createElement("button");
+  // console.log(likeButton);
+  likeButton.addEventListener("click", () => {
+    // console.log("clicked", pizza);
+    console.log(pizza.likes);
+    // update innerText of likes p tag
+    // pizza.likes += 1;
+    // likes.innerText = pizza.likes + " likes";
+    // likes.innerText = parseInt(likes.innerText) + 1 + " likes";
+    pizza.likes++;
+    likes.innerText = `${pizza.likes} likes`;
+  });
+  // likeButton.addEventListener("click", incrementLikes);
   likeButton.innerText = "❤️";
   let likes = document.createElement("p");
   likes.innerText = `${pizza.likes} likes`;
@@ -27,3 +40,36 @@ function renderPizza(pizza) {
   pizzaDiv.append(pizzaName, pizzaImg, pizzaToppings, likesDiv);
   pizzaMenu.append(pizzaDiv);
 }
+
+// When a user submits the form, the pizza appears on the DOM. No persistence necessary.
+
+// select the form
+let form = document.querySelector("#new-pizza-form");
+// add submit event to form
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // make sure submit event is working
+  // console.log("submitted");
+
+  // collect the form data
+  // select name, toppings, and image
+  let name = document.querySelector("#pizza-name").value;
+  let toppings = document.querySelector("#pizza-toppings").value;
+  let image = document.querySelector("#pizza-image").value;
+  let likes = 0;
+  // console.log(name, toppings, image);
+
+  // append pizza to the dom using renderPizza
+  // first build an object with keys name, toppings, image
+  let pizza = { name, toppings, image, likes };
+  // {name: "v", toppings: "w", image: "x"}
+  // console.log(pizza);
+  renderPizza(pizza);
+});
+
+// function incrementLikes(event) {
+//   // how do I access the pizzas likes
+//   // debugger;
+//   let likesPTag = event.target.parentElement.lastChild;
+//   likesPTag.innerText = parseInt(likesPTag.innerText) + 1 + " likes";
+// }
